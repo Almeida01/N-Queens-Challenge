fbba6062f5b8680295065dc882090145299e02ff
Gonçalo Almeida <a71262@ualg.pt>
2022-11-10 21:47:07 +0000

    Children a funcionar

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,27 @@
 package main;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 import main.models.Board;
+import main.services.Ilayout;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         Board b1 = new Board(sc.nextInt());
-        System.out.println(b1.toString());
+        List<Ilayout> c = b1.children();
+        for (int i = 0; i < c.size(); i++) {
+            Board el = (Board) c.get(i);
+            System.out.println("---- PAI " + i + " -----");
+            System.out.println(el);
+            System.out.println("---- FILHOS -----");
+            List<Ilayout> x = el.children();
+            x.forEach(System.out::println);
+
+        }
         sc.close();
     }
 }
--- src/main/models/Board.java
+++ src/main/models/Board.java
@@ -2,34 +2,54 @@ package main.models;
 
 import main.services.Ilayout;
 
-import java.util.BitSet;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLongArray;
 
-public class Board implements Ilayout {
+import static java.lang.Math.abs;
+
+public class Board implements Ilayout, Cloneable {
     private final int n;
-    private static final int dim = 0;
-    private final BitSet board;
-    private final int numberOfQueens;
+
+    /**
+     * An integer array representing the board.\n
+     * Each index of the array represents a row, where each index value represents the column where a queen is placed.
+     */
+    private final int[] board;
+    private int currentLine = 0;
 
     public Board(int n) {
         this.n = n;
-        this.dim = n*n;
-        this.board = new BitSet(dim);
-        numberOfQueens = 0;
+        this.board = new int[n];
+    }
+
+    public Board(int n, int[] board, int currentLine) {
+        this.n = n;
+        this.board = board;
+        this.currentLine = currentLine;
     }
 
     public String toString() {
         StringBuilder str = new StringBuilder();
-        int newLine = 1;
-        for (int i = 0; i < dim; i++) {
-            if (board.get(i)) str.append(" Q ");
-            else str.append(" - ");
-
-            if(newLine == 4) {
+        int row = 0, col = 0;
+        while (row < currentLine) {
+            if (col != board[row]) str.append(" - ");
+            else str.append(" Q ");
+            col++;
+            if (col == n) {
                 str.append('\n');
-                newLine = 1;
-            } else newLine++;
+                col = 0;
+                row++;
+            }
+        }
+
+        while (row < n) {
+            str.append(" - ");
+            col++;
+            if (col == n) {
+                str.append('\n');
+                col = 0;
+                row++;
+            }
         }
         return str.toString();
     }
@@ -38,14 +58,46 @@ public class Board implements Ilayout {
         return Objects.hashCode(this.board);
     }
 
+    private boolean checkDiagonalCollision(int tempQueenR, int tempQueenC, int Q2row, int Q2col) {
+        int deltaRow = abs(tempQueenR - Q2row);
+        int deltaCol = abs(tempQueenC - Q2col);
+        return deltaCol == deltaRow;
+    }
+
+    private boolean checkAxisCollision(int tempQueenR, int tempQueenC, int Q2row, int Q2col) {
+        return tempQueenR == Q2row || tempQueenC == Q2col;
+    }
+
+    /**
+     * This method checks if a given queen <code>tempQueenC</code> attacks any other queen.
+     * It iterates every other queen checking for collisions.
+     *
+     * @param tempQueenR Row of queen being placed.
+     * @return Returns a boolean value. True if it collides, false otherwise.
+     */
+    private boolean collides(int tempQueenR) {
+        int tempQueenC = board[tempQueenR]; // Column of temporary queen
+        for (int row = 0; row < currentLine; row++) {
+            int qCol = board[row];
+            if (checkAxisCollision(tempQueenR, tempQueenC, row, qCol) || checkDiagonalCollision(tempQueenR, tempQueenC, row, qCol))
+                return true;
+        }
+        return false;
+    }
+
     @Override
     public List<Ilayout> children() {
-        return null;
+        List<Ilayout> children = new ArrayList<>();
+        for (int col = 0; col < n; col++) {
+            board[currentLine] = col;
+            if (!collides(currentLine)) children.add(new Board(this.n, this.board.clone(),currentLine + 1));
+        }
+        return children;
     }
 
     @Override
     public boolean isGoal(Ilayout l) {
-        return false;
+        return currentLine == n;
     }
 
     @Override
@@ -57,4 +109,5 @@ public class Board implements Ilayout {
     public boolean isPossible(Ilayout goal) {
         return false;
     }
+
 }
